1732fb16d314a40e872d2e0940a41ff2fca4e8a1
Dmitry <dm.rubashko@gmail.com>
2021-10-30 23:56:55 +0300

    new commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import {BrowserRouter} from "react-router-dom";
 import './index.css';
 import {Provider} from 'react-redux'
 
+
+import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension/logOnlyInProduction';
+
+
 import Routes from "./Routes/Routes";
 import MainLayout from "./commonComponents/Layout/MainLayout";
 import {configureStore} from "./store/configureStore";
 
-const store = configureStore()
+const store = configureStore(devToolsEnhancer(
+  // options like actionSanitizer, stateSanitizer
+));
 
 ReactDOM.render(
   <BrowserRouter>
